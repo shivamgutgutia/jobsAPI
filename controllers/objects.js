f936b114963938d064cff17eba1518dc52b1e80e
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-15 00:50:22 +0530

    Deleted the template files that are unnecessary

--- controllers/objects.js
+++
@@ -1,7 +0,0 @@
-const objects = require("../models/objects")
-
-const getObjects = async(req,res)=>{
-    res.status(200).send("Hello World")
-}
-
-module.exports = {getObjects}
--- routers/objects.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const {getObjects} = require("../controllers/objects")
-
-router.get("/",getObjects)
-
-module.exports = router
