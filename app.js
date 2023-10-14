e07ffa7b53ccc090335ce33480e3f29547ef6c56
Shivam Gutgutia <gutgutia159@gmail.com>
2023-10-15 02:40:35 +0530

    Custom error handler

--- app.js
+++ app.js
@@ -9,7 +9,22 @@ const authRouter = require("./routers/auth")
 const jobsRouter = require("./routers/jobs")
 const auth = require("./middleware/auth")
 
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
+
 app.use(express.json())
+app.use(helmet)
+app.use(cors)
+app.use(xss)
+app.use(rateLimiter({
+    windowMs:15*60*1000,
+    max:100
+}))
+
 //app.use("./public")
 app.use("/api/v1/auth",authRouter)
 app.use("/api/v1/jobs",auth,jobsRouter)
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -4,5 +4,5 @@ module.exports= (err,req,res,next)=>{
     if(err instanceof customError){
         return res.status(err.statusCode).json({msg:err.message})
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({msg:err.message})
+    return res.status(err.statusCode||StatusCodes.INTERNAL_SERVER_ERROR).json({msg:err.message})
 }
